7374c95771cf0b445c19bd409e7023b509ba899a
anshul313 <anshul313>
2016-10-19 06:02:06 +0530

    code update

--- modules/vendor/server/controllers/vendor.server.controller.js
+++ modules/vendor/server/controllers/vendor.server.controller.js
@@ -431,7 +431,7 @@ exports.googleDataInsert = function(req, res) {
   for (var k = 1; k < 31; k++) {
 
     fs.readFile(path.resolve(__dirname,
-      'Gaziabad/Sheet1/PHYSIOTHEERAPIST/GaziyabadPHYSIOTHEERAPIST' +
+      'Gaziabad/Sheet2/PHYSIOTHEERAPIST/GaziyabadPHYSIOTHEERAPIST' +
       k + '.json'), 'utf8', function(err, data) {
 
       var jsonData = JSON.parse(data);
