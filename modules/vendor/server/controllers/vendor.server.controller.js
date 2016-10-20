33cf94d214acc6bbc1b3b3661abaa539096150df
anshul313 <anshul313>
2016-10-20 19:25:01 +0530

    codeupdate

--- modules/vendor/server/controllers/vendor.server.controller.js
+++ modules/vendor/server/controllers/vendor.server.controller.js
@@ -445,7 +445,7 @@ exports.googleDataInsert = function(req, res) {
   var l = 0;
   var asyncTasks = [];
 
-  for (var k = 36; k < 70; k++) {
+  for (var k = 71; k < 95; k++) {
 
     fs.readFile(path.resolve(__dirname,
       'files/Gurgon/PHARMACY/GurgoanPHARMACY' +
