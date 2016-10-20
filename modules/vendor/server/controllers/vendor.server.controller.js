7bd7d857521acf303b6db901da3852a7203bc47a
anshul313 <anshul313>
2016-10-20 12:56:41 +0530

    code update

--- modules/vendor/server/controllers/vendor.server.controller.js
+++ modules/vendor/server/controllers/vendor.server.controller.js
@@ -445,7 +445,7 @@ exports.googleDataInsert = function(req, res) {
   var l = 0;
   var asyncTasks = [];
 
-  for (var k = 1; k < 20; k++) {
+  for (var k = 21; k < 50; k++) {
 
     fs.readFile(path.resolve(__dirname,
       'files/noida/ATM/NoidaATM' +
