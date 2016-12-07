38c0f787ed1eefccffd815f9843260421168cc74
anshul313 <anshul313>
2016-12-07 11:41:44 +0530

    db url update

--- config/env/production.js
+++ config/env/production.js
@@ -10,7 +10,7 @@ module.exports = {
   // Binding to 127.0.0.1 is safer in production.
   host: process.env.HOST || '0.0.0.0',
   db: {
-    uri: 'mongodb://chiblee:chiblee1212@54.169.192.5:12528/chiblee',
+    uri: 'mongodb://139.59.9.200:12528/chiblee',
     options: {
       user: '',
       pass: ''
